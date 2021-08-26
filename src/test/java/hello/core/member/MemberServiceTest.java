558df5db58bdbbf00c50bb34bb566bf191d9e0e5
Donggeun <thsdimaker@gmail.com>
2021-08-26 11:28:54 +0900

    init

---
+++ src/test/java/hello/core/member/MemberServiceTest.java
@@ -0,0 +1,38 @@
+package hello.core.member;
+
+import hello.core.AppConfig;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MemberServiceTest {
+
+    MemberService memberService;
+
+    @BeforeEach
+    public void beforeEach(){
+        AppConfig appConfig = new AppConfig();
+        memberService = appConfig.memberService();
+    }
+
+
+    @Test
+    void join() {
+
+        //given : 환경 ~가 주어졌을 때
+        Member member = new Member(1L, "memberA", Grade.VIP);
+
+        //when : 이렇게 했을 때
+        memberService.join(member);
+        Member findMember = memberService.findMember(1L);
+
+        //then : 이렇게 된다.
+        Assertions.assertThat(member).isEqualTo(findMember);
+    }
+
+    @Test
+    void findMember() {
+    }
+}
